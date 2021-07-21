9dfb064e1e3a41d3f85c3356cadd77947a510cc3
stevensll <deltaorb637@gmail.com>
2021-07-21 11:49:35 -0400

    debugging lab4b

--- labs/lab4/lab4b.py
+++ labs/lab4/lab4b.py
@@ -42,14 +42,49 @@ def start():
     print(">> Lab 4B - LIDAR Wall Following")
 
 
+
 def update():
     """
     After start() is run, this function is run every frame until the back button
     is pressed
     """
+    
+    # right_dist = rc_utils.get_lidar_closest_point(scan, (80,100))[1]
+    # left_dist = rc_utils.get_lidar_closest_point(scan, (260,280))[1]
+    angle = 0
+    speed = 0
     # TODO: Follow the wall to the right of the car without hitting anything.
-    pass
 
+    scan = rc.lidar.get_samples()
+    sectors = 10
+    window_length = 180
+    window_start = -90
+    farthest = [0, 0]
+    windows = [[window_start +  w *window_length//sectors, window_start +  (w+1) *window_length//sectors] for w in range(sectors)]
+    
+    for window in windows:
+        temp = rc_utils.get_lidar_closest_point(scan, window)
+        if temp[1] > farthest [1]:
+            farthest = temp
+
+    for window in windows:
+        temp = farthest[0]
+        temp = temp - 360 if temp > 270 else temp
+        if temp in range(window[0],window[1]):
+            angle = (window[0] + window[1]) / 2
+            print(temp, angle)
+    
+    angle = rc_utils.remap_range(angle, window_start, window_start+window_length, -1, 1, True) 
+    angle = rc_utils.clamp(angle*2,-1,1)
+    speed = 0.45
+    
+    rc.drive.set_speed_angle(speed, angle)
+
+    if rc.controller.is_down(rc.controller.Button.B):
+        # print("Front:", front_dist, "Back:", back_dist, "Left:", left_dist, "Right", right_dist)
+        print("Angle:", angle,)
+    if rc.controller.is_down(rc.controller.Button.A):
+        print("Windows:", windows)
 
 ########################################################################################
 # DO NOT MODIFY: Register start and update and begin execution
